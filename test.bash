1cbccfd1e64734047c375e02065afb5439176223
harus <cleary.yokot@gmail.com>
2023-11-28 15:16:04 +0900

    プログラムの修正

--- test.bash
+++ test.bash
@@ -7,9 +7,8 @@ ng () {
       res=1
 }
 
-　
 res=0
-　
+
 out=$(seq 7 |./plus)
 [ "${out}" = "合計値:28
 even number
@@ -23,11 +22,10 @@ odd number
 out=$(echo あ | ./plus)
   [ "$?" = 1 ]      || ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
-  
 out=$(echo | ./plus) 
   [ "$?" = 1 ]      || ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
-  　
-[ "$res" = 0 ] && echo OK        
+  
+  [ "$res" = 0 ] && echo OK        
 exit $res
 
