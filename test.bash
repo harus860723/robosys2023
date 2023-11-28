d8372df7ac07885f93b40c9129c773d15ca8ec06
harus <cleary.yokot@gmail.com>
2023-11-28 21:21:04 +0900

    テストの修正

--- test.bash
+++ test.bash
@@ -3,29 +3,29 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-      echo NG at Line $1
-      res=1
+	echo NG at Line $1
+	res=1
 }
 
 res=0
 
-out=$(seq 7 |./plus)
+out=$(seq 7 | ./plus)
 [ "${out}" = "合計値:28
 even number
 値を2で割った数: 14.0" ] || ng ${LINENO}
 
-out=$(seq 5 |./plus)
+out=$(seq 5 | ./plus)
 [ "${out}" = "合計値:15
 odd number
 値に2を掛けた数: 30" ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-out=$(echo | ./plus) 
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-  
-  [ "$res" = 0 ] && echo OK        
-exit $res
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
