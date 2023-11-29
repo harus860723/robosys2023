1cd258f397c92fb58711f5141c57b7c2b033712a
harus <cleary.yokot@gmail.com>
2023-11-29 10:18:16 +0900

    テストの修正

--- test.bash
+++ test.bash
@@ -17,15 +17,15 @@ even number
 out=$(seq 5 | ./plus)
 [ "${out}" = "合計値:15
 odd number
-値に2を掛けた数: 30" ] || ng ${LINENO}
+値に2を掛けた数: 30" ]   || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]             || ng ${LINENO}
+[ "${out}" = "" ]        || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]             || ng ${LINENO}
+[ "${out}" = "" ]        || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
